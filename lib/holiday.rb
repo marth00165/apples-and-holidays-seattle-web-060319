bffdc589884790caccb2d8ce9b1b678b7f87a1a7
Rohit Pratti <rohit.pratti@gmail.com>
2019-05-18 03:17:37 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -80,9 +80,11 @@ holiday_hash.each do |season, holiday|
     end
   end
   
-  madara
+  
   
 end
+madara
+
 
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
